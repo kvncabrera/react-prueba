551a458fefce510f5d6037c532f67fc64c9c0ec9
kvncabrera <165654156+kvncabrera@users.noreply.github.com>
2024-07-19 13:14:25 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,55 +1,99 @@
+// const express = require('express');
+// const cors = require('cors');
+// const bunyan = require('bunyan');
+// const guiasRouter = require('./router/guiasRouter');
+// const usuariosRouter = require('./router/usuariosRouter');
+// const bodyParser = require('body-parser');
+
+
+// const PORT = process.env.PORT || 3000;
+
+
+
+// const app = express();
+
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// app.use(express.json());
+// app.use(express.urlencoded({extended:true}))
+
+// const logger = bunyan.createLogger({name: 'Proyecto museo'});
+
+
+
+// app.use('/guias',guiasRouter);
+// app.use('/usuarios',usuariosRouter);
+
+
+
+// app.get('/', (req , res , next)=>{
+//     try{
+//         res.status(200).json('Haciendo GET en /')
+//     } catch(error){
+//         next(error)
+//     }
+// })
+
+
+// app.use((req,res)=>{
+//     res.status(404).json({mensaje: 'No se ha encontrado la ruta'})
+// })
+
+// app.use( (err,req,res,next)=>{
+//     res.status(500).json({mensaje: err})
+// } )
+
+
+
+
+// app.listen(3000, ()=>{
+
+//     logger.info('Servidor de express levantado')
+
+// })
+
+
+
+
 const express = require('express');
 const cors = require('cors');
 const bunyan = require('bunyan');
 const guiasRouter = require('./router/guiasRouter');
 const usuariosRouter = require('./router/usuariosRouter');
 const bodyParser = require('body-parser');
-
-
-const PORT = process.env.PORT || 3000;
-
-
+require('dotenv').config();
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
-
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
-const logger = bunyan.createLogger({name: 'Proyecto museo'});
+const logger = bunyan.createLogger({ name: 'Proyecto museo' });
 
+app.use('/guias', guiasRouter);
+app.use('/usuarios', usuariosRouter);
 
-
-app.use('/guias',guiasRouter);
-app.use('/usuarios',usuariosRouter);
-
-
-
-app.get('/', (req , res , next)=>{
-    try{
-        res.status(200).json('Haciendo GET en /')
-    } catch(error){
-        next(error)
+app.get('/', (req, res, next) => {
+    try {
+        res.status(200).json('Haciendo GET en /');
+    } catch (error) {
+        next(error);
     }
-})
+});
 
+app.use((req, res) => {
+    res.status(404).json({ mensaje: 'No se ha encontrado la ruta' });
+});
 
-app.use((req,res)=>{
-    res.status(404).json({mensaje: 'No se ha encontrado la ruta'})
-})
+app.use((err, req, res, next) => {
+    logger.error(err);
+    res.status(500).json({ mensaje: 'Error interno del servidor' });
+});
 
-app.use( (err,req,res,next)=>{
-    res.status(500).json({mensaje: err})
-} )
-
-
-
-
-app.listen(3000, ()=>{
-
-    logger.info('Servidor de express levantado')
-
-})
+module.exports = app;
